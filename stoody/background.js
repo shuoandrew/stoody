2aa3bdc8334a9acfea87e080bba27e0eee3902c6
shuoandrew <82245268+shuoandrew@users.noreply.github.com>
2023-03-15 23:00:12 -0400

    Delete stoody directory

--- stoody/background.js
+++
@@ -1,6 +0,0 @@
-//function changeBackground(){
-//    document.querySelector('.contentBox').style.backgroundColor = 'purple;'
-//}
-
-var randomColor = Math.floor(Math.random()*16777215).toString(16);
-document.getElementById("mainButton").style.backgroundColor = '#' + randomColor;
